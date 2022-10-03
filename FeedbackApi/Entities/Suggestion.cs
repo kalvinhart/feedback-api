ede7d8296e95f4f8da07e4756eb1acbddb603467
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-03 13:40:26 +0100

    Add project files.

---
+++ FeedbackApi/Entities/Suggestion.cs
@@ -0,0 +1,13 @@
+﻿namespace FeedbackApi.Entities
+{
+    public class Suggestion
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public int Upvotes { get; set; }
+
+
+    }
+}
