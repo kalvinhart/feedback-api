dfa22a89f878cab019f98bbd98ae717d9e3f9245
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-03 14:02:43 +0100

    added user id field

--- FeedbackApi/Entities/Suggestion.cs
+++ FeedbackApi/Entities/Suggestion.cs
@@ -8,5 +8,6 @@
         public string Category { get; set; } = null!;
         public int Upvotes { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
+        public int UserId { get; set; }
     }
 }
