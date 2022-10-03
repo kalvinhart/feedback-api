ac3306decdeaaa4c37d5c95aefebf55dfcf84896
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-03 14:01:26 +0100

    added user model

---
+++ FeedbackApi/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FeedbackApi.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
