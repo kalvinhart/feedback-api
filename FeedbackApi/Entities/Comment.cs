74461b88c44eafc1c1173c6357c9dc4e98b78089
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-03 15:47:22 +0100

    added new migration and added seed class

--- FeedbackApi/Entities/Comment.cs
+++ FeedbackApi/Entities/Comment.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Content { get; set; } = null!;
 
-        public string Author { get; set; } = null!;
+        public User User { get; set; } = null!;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public List<Comment> Replies { get; set; } = new List<Comment>();
         public int SuggestionId { get; set; }
--- FeedbackApi/Entities/User.cs
+++ FeedbackApi/Entities/User.cs
@@ -7,6 +7,13 @@
         public string Username { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+        public Role Role { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public enum Role
+    {
+        Admin,
+        User
     }
 }
