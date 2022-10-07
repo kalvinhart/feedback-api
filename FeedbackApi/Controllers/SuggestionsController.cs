f62424908f7b01a80ae45046074e960591cb057f
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-07 14:27:20 +0100

    added method to get individual suggestion

--- FeedbackApi/Controllers/SuggestionsController.cs
+++ FeedbackApi/Controllers/SuggestionsController.cs
@@ -25,6 +25,22 @@ namespace FeedbackApi.Controllers
             return Ok(suggestions);
         }
 
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            var suggestion = await _context.Suggestions
+                .Include(s => s.Comments)
+                .ThenInclude(c => c.Replies)
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+            if (suggestion == null) return NotFound("Suggestion ID does not exist.");
+
+            return Ok(suggestion);
+        }
+
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
