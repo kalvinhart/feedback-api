e2530ff6d051416d665e0e2997b1754e5101cddb
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-07 16:43:53 +0100

    added basic DTOs

---
+++ FeedbackApi/DTOs/Comment/GetCommentDto.cs
@@ -0,0 +1,15 @@
+﻿using FeedbackApi.DTOs.User;
+using FeedbackApi.Entities;
+
+namespace FeedbackApi.DTOs.Comment
+{
+    public class GetCommentDto
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public List<CommentReply> Replies { get; set; } = new List<CommentReply>();
+        public UserDto User { get; set; } = null!;
+        public int SuggestionId { get; set; }
+    }
+}
---
+++ FeedbackApi/DTOs/User/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FeedbackApi.DTOs.User
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
