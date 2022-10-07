7555cc4e592175c0f6d7d4825efe1b0912ebd80f
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-07 12:49:12 +0100

    adjusted relationship properties

--- FeedbackApi/Entities/Comment.cs
+++ FeedbackApi/Entities/Comment.cs
@@ -4,10 +4,9 @@
     {
         public int Id { get; set; }
         public string Content { get; set; } = null!;
-
-        public User User { get; set; } = null!;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public List<Comment> Replies { get; set; } = new List<Comment>();
+        public User User { get; set; } = null!;
         public int SuggestionId { get; set; }
     }
 }
---
+++ FeedbackApi/Migrations/20221007114752_AdjustRelationships.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FeedbackApi.Migrations
+{
+    public partial class AdjustRelationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Suggestions_Users_UserId",
+                table: "Suggestions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Suggestions_UserId",
+                table: "Suggestions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Suggestions_UserId",
+                table: "Suggestions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Suggestions_Users_UserId",
+                table: "Suggestions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
