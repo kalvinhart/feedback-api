3c8ccae5b4e7832a1900dab9ffabec6b2704c232
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-07 13:46:17 +0100

    added method to post comments

--- FeedbackApi/Controllers/CommentsController.cs
+++ FeedbackApi/Controllers/CommentsController.cs
@@ -18,7 +18,7 @@ namespace FeedbackApi.Controllers
         }
 
         [HttpGet]
-        [Route("{suggestionId}")]
+        [Route("{suggestionId}", Name = "GetComments")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetComments(int suggestionId)
@@ -29,6 +29,27 @@ namespace FeedbackApi.Controllers
 
             var comments = await _context.Comments.Where(c => c.SuggestionId == suggestionId).OrderBy(c => c.CreatedAt).ToListAsync();
             return Ok(comments);
+        }
+
+        [HttpPost]
+        [Route("{suggestionId}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> PostComment(int suggestionId, Comment comment)
+        {
+            var suggestion = await _context.Suggestions.FindAsync(suggestionId);
+
+            if (suggestion == null) return NotFound("Suggestion ID does not exist.");
+
+            var newComment = new Comment
+            {
+                SuggestionId = suggestionId,
+                Content = comment.Content,
+                User = comment.User
+            };
+
+            await _context.Comments.AddAsync(newComment);
+            return CreatedAtRoute("GetComments", newComment);
         }
     }
 }
